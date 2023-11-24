275f81e37b7882ae7221a12dc8c9d465b8479e0a
Mizu <psdk55052004@gmail.com>
2023-11-24 11:50:25 +0800

    新增 初始化 howler

--- src/components/Demo1.vue
+++ src/components/Demo1.vue
@@ -1,8 +1,41 @@
 <template>
-  hi
+  <main class="bg-gainsboro"
+        style="width:100vw;
+               height:100vh;">
+  </main>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+import { Howl, Howler } from 'howler'
+
+console.log('Howl', Howl)
+console.log('Howler', Howler)
+
+const myMp3 = ref(null)
+
+onMounted(() => {
+  howlerInit()
+})
+
+function howlerInit () {
+  myMp3.value = new Howl({
+    // src: ['sound.webm', 'sound.mp3', 'sound.wav'],
+    src: ['src/assets/files/mp3.mp3'],
+    // autoplay: true,
+    // loop: true,
+    // volume: 0.5,
+    // html5: true // 串流音訊（用於即時音訊或大檔案）
+    // sprite: {
+    //   blast: [0, 3000],
+    //   laser: [4000, 1000],
+    //   winner: [6000, 5000]
+    // }
+    onend: function () {
+      console.log('Finished!')
+    }
+  })
+}
 
 </script>
 
